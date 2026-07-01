d78ea2ec1fa785f24f407bfbc9abb2dac876f1a5
Carlos-iso <carlosbalak2002@gmail.com>
2026-07-01 20:14:52 -0300

    feat: remove controller.db

--- routes/index.js
+++ routes/index.js
@@ -10,4 +10,13 @@ router.get("/", function (req, res, next) {
 	});
 });
 
+router.get("/api", function (req, res, next) {
+	res.send({
+		endpoint: "/api",
+		banco: "PostgreSQL",
+		description: "API para o projeto Saúde em Linhas",
+		environment: process.env.NODE_ENV || "development",
+	});
+});
+
 module.exports = router;
